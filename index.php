443ae570cb76960d913a3857b01dbf9bdefa06c5
eduBado <edubado08@gmail.com>
2015-11-16 19:16:25 -0200

    Update index.php
    Arquivo atualizado

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html lang="pt-br">
+<html>
     <head>
         <meta charset="UTF-8">
-        <title>Site Institucional do SENAI - Brusque</title>
+        <title>Site Institucional do Senai - Brusque</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/estilo.css"/>
+
+        <!-- Javascript-->
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="js/bootstrap.js" ></script>
+        <script src="js/meuscript.js"></script>
+        <link rel="shortcut icon" href="http://sc.senai.br/sites/default/files/favicon.ico" type="image/vnd.microsoft.icon">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container-fluid">
+            <header class="jumbotron text-center" id="topo">
+                <h1>SENAI/SC Brusque</h1>
+                <form role="form">
+                     <div class="form-inline">
+                    <div class="input-group input-group-sm">
+                        <span class="input-group-addon ">
+                            <span class="glyphicon glyphicon-envelope"> </span>
+                        </span>
+                       
+                            <input type="email" class="form-control" id="email" placeholder="Digite seu E-mail" >
+                        </div>
+                        <button type="submit" class="btn btn-success">Cadastrar</button>
+                    </div>
+            </header>
+        </div>   
     </body>
 </html>
+
