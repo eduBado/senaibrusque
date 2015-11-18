a65bb91b27fe39eb853be3acbff853d8180723bd
Aluno <Aluno@10.1.20.63>
2015-11-18 03:25:38 -0200

    closes #19

--- index.php
+++ index.php
@@ -11,12 +11,17 @@
         <!-- Javascript-->
         <script src="js/jquery-1.11.3.min.js"></script>
         <script src="js/bootstrap.js" ></script>
-        
+
+
         <link rel="shortcut icon" href="http://sc.senai.br/sites/default/files/favicon.ico" type="image/vnd.microsoft.icon">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="js/jssor.slider.mini.js"></script>
+        
+
     </head>
-    <body data-spy="scroll" data-target=".navbar" data-offset="20">
+    <body data-spy="scroll" data-target=".navbar" data-offset="50">
 
         <header class="jumbotron text-center" id="topo">
             <h1>SENAI/SC - Brusque</h1>
@@ -37,7 +42,7 @@
 
 
 
-        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="300"  >
+        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="100"  >
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -76,81 +81,181 @@
         <!-- CARROSSEL -->
         <div id="section1" class="container-fluid">
             <h1>Inicio</h1>
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                    <li data-target="#myCarousel" data-slide-to="3"></li>
-                </ol>
 
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img src="http://files.lucianoifsp.com.br/200000419-01bb203ad4/senai_1.jpg"Chania">
-
+            <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden; visibility: hidden;">
+                <!-- Loading Screen -->
+                <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
+                    <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+                    <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+                </div>
+                <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden;">
+                    <div data-p="112.50" style="display: none;">
+                        <img data-u="image" src="img/02.jpg" />
                     </div>
-
-                    <div class="item">
-                        <img src="http://lagoagrande.net/wp-content/uploads/2013/05/SENAI-PE-EM-A%C3%87%C3%83O.jpg" alt="Chania">
-
+                    <div data-p="112.50" style="display: none;">
+                        <img data-u="image" src="img/04.jpg" />
                     </div>
-
-                    <div class="item">
-                        <img src="http://www.cursosgratuitossenai2015.com.br/wp-content/uploads/2015/01/Alunos-senai.jpg" alt="Flower">
-
+                    <div data-p="112.50" style="display: none;">
+                        <img data-u="image" src="img/05.jpg" />
                     </div>
-
-                    <div class="item">
-                        <img src="http://arquivos.portaldaindustria.com.br/app/foto_133053580552/2012/06/27/83/20120627172856395099u.jpg" alt="Flower">
-
+                    <div data-p="112.50" style="display: none;">
+                        <img data-u="image" src="img/09.jpg" />
                     </div>
                 </div>
-
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <!-- Bullet Navigator -->
+                <div data-u="navigator" class="jssorb05" style="bottom:16px;right:6px;" data-autocenter="1">
+                    <!-- bullet navigator item prototype -->
+                    <div data-u="prototype" style="width:16px;height:16px;"></div>
+                </div>
+                <!-- Arrow Navigator -->
+                <span data-u="arrowleft" class="jssora12l" style="top:123px;left:0px;width:30px;height:46px;" data-autocenter="2"></span>
+                <span data-u="arrowright" class="jssora12r" style="top:123px;right:0px;width:30px;height:46px;" data-autocenter="2"></span>
+                <a href="http://www.jssor.com" style="display:none">Jssor Slider</a>
             </div>
+
+            <!-- #endregion Jssor Slider End -->
+
         </div>
 
         <div id="section2" class="container-fluid">
-            <h1>Cursos</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+
+            <div class="row">
+                
+                <!-- CURSO UM -->
+                <div class="col-md-4 m-b-lg">
+                    <div class="panel panel-default panel-profile m-b-0">
+                        <div class="panel-body text-center">
+                            <img class="panel-profile-img" src="//cdn.shopify.com/s/files/1/0691/5403/t/108/assets/avatar-fat.jpg?14626024167487121591">
+                            <h5 class="panel-title">Jacob Thornton</h5>
+                            <p class="m-b">Star trek flow so they klingon me. I wrote this here code. Two times.</p>
+                            
+                            <!-- BOTÃO -->
+                            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+                            <!-- Modal -->
+                            <div id="myModal" class="modal fade" role="dialog">
+                                <div class="modal-dialog">
+
+                                    <!-- Modal content-->
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">Modal Header</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Some text in the modal.</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- CURSO DOIS -->
+                <div class="col-md-4 m-b-lg">
+                    <div class="panel panel-default panel-profile m-b-0">
+                        <div class="panel-body text-center">
+                            <img class="panel-profile-img" src="//cdn.shopify.com/s/files/1/0691/5403/t/108/assets/avatar-fat.jpg?14626024167487121591">
+                            <h5 class="panel-title">Jacob Thornton</h5>
+                            <p class="m-b">Star trek flow so they klingon me. I wrote this here code. Two times.</p>
+                            
+                            <!-- BOTÃO -->
+                            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+                            <!-- Modal -->
+                            <div id="myModal" class="modal fade" role="dialog">
+                                <div class="modal-dialog">
+
+                                    <!-- Modal content-->
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">Modal Header</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Some text in the modal.</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- CURSO TRES -->
+                <div class="col-md-4 m-b-lg">
+                    <div class="panel panel-default panel-profile m-b-0">
+                        <div class="panel-body text-center">
+                            <img class="panel-profile-img" src="//cdn.shopify.com/s/files/1/0691/5403/t/108/assets/avatar-fat.jpg?14626024167487121591">
+                            <h5 class="panel-title">Jacob Thornton</h5>
+                            <p class="m-b">Star trek flow so they klingon me. I wrote this here code. Two times.</p>
+                            
+                            <!-- BOTÃO -->
+                            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+                            <!-- Modal -->
+                            <div id="myModal" class="modal fade" role="dialog">
+                                <div class="modal-dialog">
+
+                                    <!-- Modal content-->
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">Modal Header</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Some text in the modal.</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
         </div>
-        <div id="section3" class="container-fluid">
-            <h1>Noticias</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section4" class="container-fluid">
-            <h1>Fotos</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section5" class="container-fluid">
-            <h1>Videos</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section6" class="container-fluid">
-            <h1>Contato</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section7" class="container-fluid">
-            <h1>Localização</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-<script src="js/meuscript.js"></script>
-    </body>
+    </div>
+
+    <div id="section3" class="container-fluid">
+        <h1>Noticias</h1>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+    </div>
+    <div id="section4" class="container-fluid">
+        <h1>Fotos</h1>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+    </div>
+    <div id="section5" class="container-fluid">
+        <h1>Videos</h1>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+    </div>
+    <div id="section6" class="container-fluid">
+        <h1>Contato</h1>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+    </div>
+    <div id="section7" class="container-fluid">
+        <h1>Localização</h1>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+    </div>
+    <script src="js/meuscript.js"></script>
+</body>
 </html>
 
--- js/meuscript.js
+++ js/meuscript.js
@@ -5,25 +5,69 @@
  */
 
 // Add scrollspy to <body>
-$('body').scrollspy({target: ".navbar", offset: 50}); 
+//$('body').scrollspy({target: ".navbar", offset: 50}); 
 
 // Add smooth scrolling to all links inside a navbar
-$("#myNavbar a").on('click', function(event){
+$("#myNavbar a").on('click', function (event) {
 
-  // Prevent default anchor click behavior
-  event.preventDefault();
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-  // Store hash (#)
-  var hash = this.hash;
+    // Store hash (#)
+    var hash = this.hash;
 
-  // Using jQuery's animate() method to add smooth page scroll
-  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
-  $('html, body').animate({
-    scrollTop: $(hash).offset().top
-  }, 700, function(){
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 700, function () {
 
-    // Add hash (#) to URL when done scrolling (default click behavior)
-    window.location.hash = hash;
-  });
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+    });
 });
 
+
+
+
+jQuery(document).ready(function ($) {
+
+    var jssor_1_SlideshowTransitions = [
+        {$Duration: 1200, $Opacity: 2}
+    ];
+
+    var jssor_1_options = {
+        $AutoPlay: true,
+        $SlideshowOptions: {
+            $Class: $JssorSlideshowRunner$,
+            $Transitions: jssor_1_SlideshowTransitions,
+            $TransitionsOrder: 1
+        },
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        },
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$
+        }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    //responsive code begin
+    //you can remove responsive code if you don't want the slider scales while window resizes
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 600);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    //responsive code end
+});
